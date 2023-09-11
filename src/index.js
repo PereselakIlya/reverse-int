9a2fd471a06b90ed44b139d87706980965f36083
Pereselakilya <104826618+PereselakIlya@users.noreply.github.com>
2023-09-11 10:31:54 +0300

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+    var output="";
+    var nstr=n.toString();
+    var nchararr=nstr.split('');
+    for(var i=nchararr.length;i>=0;--i){
+        output+=nchararr[i];
+    }
+    output=output.replace(undefined,"");
+    output=output.replace(/-/i,"");
+    return output;
 }
